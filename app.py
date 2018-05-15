9496d024ff26a647dd36b31432da78dc110df3d4
Mohib Jafri <mjafri@Mohibs-MacBook-Pro.local>
2018-05-15 13:32:33 -0700

    API Key Refractoring

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import send_file, Flask, request
 app = Flask(__name__)
 from weather import weather
 
-# Sample: http://127.0.0.1:5000/weather?city=Davis&state=CA&reportHours=12&key=a38755057259545e
+# Sample: http://127.0.0.1:5000/weather?city=Davis&state=CA&reportHours=12
 @app.route('/weather')
 def get_image():
 
     city = request.args.get('city')
     state = request.args.get('state')
     reportHours = int(request.args.get('reportHours'))
-    key = request.args.get('key')
 
-    filePath = weather(state = state, city = city, reportHours = reportHours, key = key).createGraph()
+    filePath = weather(state = state, city = city, reportHours = reportHours).createGraph()
     return send_file(filePath, mimetype='image/png')
 
 if __name__ == '__main__':
